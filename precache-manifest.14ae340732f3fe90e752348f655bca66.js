31ee84c361b4d50c140e5ac39f2932ce2d9a54e0
gurpartb <gurpartap.s.bhatti@gmail.com>
2019-03-26 11:40:14 -0600

    Updates

---
+++ precache-manifest.14ae340732f3fe90e752348f655bca66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0b4c3ccf2728cb480d3d",
+    "url": "/static/js/main.b62cefb8.chunk.js"
+  },
+  {
+    "revision": "0571b69cddc311e66ec0",
+    "url": "/static/js/2.b934461c.chunk.js"
+  },
+  {
+    "revision": "0b4c3ccf2728cb480d3d",
+    "url": "/static/css/main.6334a660.chunk.css"
+  },
+  {
+    "revision": "a88794b75951419aef7bb0740d627d3a",
+    "url": "/index.html"
+  }
+];
